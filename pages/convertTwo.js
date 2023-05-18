c5a72db45b0765be3f600ff7353fcc950202c52e
ModerRAS <adqewsx@gmail.com>
2023-05-18 10:48:46 +0800

    添加自动下载OCR依赖的功能
    (cherry picked from commit f8f7304df187ce529a832a897a00fa083ddfb724)

--- pages/convertTwo.js
+++ pages/convertTwo.js
@@ -66,16 +66,26 @@ async function SelectTargetFolder() {
   return file;
 }
 
-async function StartConvert(SourceFolderFiles, TargetFolder, TargetFileNames) {
+async function StartConvert(SourceFolderFiles, TargetFolder, TargetFileNames, setConvertState) {
   let Thermal = new Map();
+  
+  try {
+    setConvertState("正在准备OCR依赖")
+    let ret = await invoke("prepare_ocr_lib")
+  } catch (error) {
+
+  }
+
   if (TargetFileNames.length === SourceFolderFiles.length) {
     for (let index = 0; index < TargetFileNames.length; index++) {
       const TargetFileName = `${TargetFolder}\\${TargetFileNames[index]}.jpg`;
       const SourceFileName = SourceFolderFiles[index];
 
+      setConvertState("正在复制文件")
       await invoke("copy_file", { from: SourceFileName, to: TargetFileName });
 
       try {
+        setConvertState(`正在读取温度（第${index+1}个）`)
         let ret = await invoke("read_thermal", { image_path: SourceFileName })
         console.log(`Thermal is :${ret}`)
         Thermal.set(TargetFileNames[index], ret)
@@ -87,33 +97,6 @@ async function StartConvert(SourceFolderFiles, TargetFolder, TargetFileNames) {
   } else {
     await ask("选择的文件数目不对应", { title: "错误", type: "warning" });
   }
-}
-
-async function TestTesseract() {
-  let path = convertFileSrc('resources/test.jpg');
-  console.log(path)
-  let ret = await invoke("read_thermal", { image_path: "C:\\Users\\adqew\\Pictures\\红外\\1000kVT615ACF交流滤波器电流互感器器CT5A相.jpg" })
-  console.log(ret)
-  // const worker = await createWorker({
-  //   workerPath: convertFileSrc('resources/tesseract.js/worker.min.js'),
-  //   langPath: convertFileSrc('resources/tessdata/'),
-  //   corePath: convertFileSrc('resources/tesseract.js-core/tesseract-core.wasm.js'),
-  //   logger: m => console.log(m),
-  // });
-  // await worker.loadLanguage('eng');
-  // await worker.initialize('eng');
-  // const { data: { text } } = await worker.recognize(convertFileSrc('resources/test2.jpg'));
-  // console.log(text);
-  // await worker.terminate();
-  // await ocr.init(convertFileSrc('resources/ocr/ch_PP-OCRv2_det_fuse_activation/model.json'), convertFileSrc('resources/ocr/ch_PP-OCRv2_rec_fuse_activation/model.json'));
-  // await ocr.init()
-  // const img = document.createElement("img");
-  // img.src = convertFileSrc('resources/test.jpg')
-  // img.crossOrigin='anonymous'
-
-  // const res = await ocr.recognize(img);
-  // console.log(res.text);
-  // console.log(res.points);
 }
 
 function MergeLines(SourceFolderFiles, TargetFolder, TargetFileNames) {
@@ -144,6 +127,7 @@ export default function Page() {
   const [TargetFolder, setTargetFolder] = useState("");
   const [TargetFileNames, setTargetFileNames] = useState([""]);
   const [AllFilePaths, setAllFilePaths] = useState([[""]]);
+  const [ConvertState, setConvertState] = useState("空闲")
   useEffect(() => {
     setAllFilePaths(MergeLines(SourceFolderFiles, TargetFolder, TargetFileNames))
   }, [SourceFolderFiles, TargetFolder, TargetFileNames]);
@@ -167,12 +151,15 @@ export default function Page() {
         />
         <Button
           handler={async () =>
-            await StartConvert(SourceFolderFiles, TargetFolder, TargetFileNames)
+            await StartConvert(SourceFolderFiles, TargetFolder, TargetFileNames, setConvertState)
             // await TestTesseract()
           }
           name={"开始转换"}
           description={"按下进行文件名修改"}
         />
+        <div className="border-collapse border border-green-800 table-auto col-span-4">
+          当前状态：{ConvertState}
+        </div>
         <table className="border-collapse border border-green-800 table-auto col-span-4">
           <thead>
             <tr>
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,9 @@
 use std::env;
 use std::fs;
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
+use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 use std::vec;
@@ -29,13 +31,16 @@ use image::Pixel;
 use rand::{thread_rng, Rng};
 use rnglib::{Language, RNG};
 use serde::Serialize;
+use tauri::async_runtime;
 use tauri::generate_handler;
 use tauri::Manager;
 
 use serde::Deserialize;
 use serde_json;
 
-use directories::{BaseDirs, UserDirs, ProjectDirs};
+use directories::{BaseDirs, ProjectDirs, UserDirs};
+
+use futures_util::StreamExt;
 
 #[derive(Deserialize)]
 struct OcrResult {
@@ -73,10 +78,116 @@ fn main() {
             read_excel_lines,
             get_image_from_directory,
             copy_file,
-            read_thermal
+            read_thermal,
+            prepare_ocr_lib
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command(rename_all = "snake_case")]
+async fn prepare_ocr_lib() {
+    if check_if_ocr_lib_downloaded() {
+        return;
+    }
+    let download_path = get_download_path().unwrap();
+    let data_path = get_ocr_path().unwrap();
+    download_files("https://ghp.miaostay.com/https://github.com/ModerRAS/ITMPC-Public/releases/download/ocr_lib/PaddleOCR-json.v1.2.1.zip", download_path.clone()).await.unwrap();
+    unzip_ocr_lib(download_path, data_path);
+}
+
+fn check_if_ocr_lib_downloaded() -> bool {
+    match get_ocr_path() {
+        Ok(ocr_path) => return ocr_path.join("PaddleOCR_json.exe").exists(),
+        Err(_) => return false,
+    }
+}
+
+async fn download_files(url: &str, path: PathBuf) -> Result<(), ()> {
+    let mut file = File::create(path).unwrap();
+    println!("Downloading {}...", url);
+
+    let mut stream = reqwest::get(url).await.unwrap().bytes_stream();
+
+    while let Some(chunk_result) = stream.next().await {
+        let chunk = chunk_result.unwrap();
+        file.write_all(&chunk);
+    }
+
+    file.flush().unwrap();
+
+    println!("Downloaded {}", url);
+    Ok(())
+}
+
+fn get_download_path() -> Result<PathBuf, String> {
+    if let Some(proj_dirs) = UserDirs::new() {
+        if let Some(ppocr_dirs) = proj_dirs.download_dir() {
+            let fpath = ppocr_dirs.join("PaddleOCR-json.v1.2.1.zip");
+            let fname = fpath;
+            return Ok(fname);
+        }
+    }
+    return Err(String::from("Error"));
+}
+
+fn get_ocr_path() -> Result<PathBuf, String> {
+    if let Some(ppocr_dir) = ProjectDirs::from("com", "com.miaostay.itmpc", "PaddleOCR-json") {
+        return Ok(ppocr_dir.data_local_dir().to_path_buf());
+    }
+    return Err(String::from("Error"));
+}
+
+fn unzip_ocr_lib(zip_path: PathBuf, to: PathBuf) -> i32 {
+    let file = fs::File::open(zip_path.as_path()).unwrap();
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+        let outpath = to.join(outpath);
+
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("File {i} comment: {comment}");
+            }
+        }
+
+        if (*file.name()).ends_with('/') {
+            println!("File {} extracted to \"{}\"", i, outpath.display());
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                outpath.display(),
+                file.size()
+            );
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(p).unwrap();
+                }
+            }
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+
+        // Get and Set permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+            }
+        }
+    }
+
+    0
 }
 
 #[tauri::command(rename_all = "snake_case")]
@@ -153,8 +264,6 @@ fn copy_file(from: &str, to: &str) -> Result<u64, String> {
 #[tauri::command(rename_all = "snake_case")]
 fn write_to_excel() {
     let mut workbook = Workbook::new();
-
-
 }
 
 #[tauri::command(rename_all = "snake_case")]
@@ -165,14 +274,20 @@ fn read_thermal(image_path: &str) -> Result<f32, ()> {
     let temp_name = rng.generate_name();
     let output_path = temp_dir.join(temp_name + ".jpg");
     match clip_picture(&PathBuf::from(image_path), &output_path) {
-        Ok(_) => match detect_image_thermal_ocr(output_path.clone().into_os_string().into_string().unwrap().as_str()) {
+        Ok(_) => match detect_image_thermal_ocr(
+            output_path
+                .clone()
+                .into_os_string()
+                .into_string()
+                .unwrap()
+                .as_str(),
+        ) {
             Ok(t) => {
                 match fs::remove_file(&output_path) {
                     Ok(_) => return Ok(t),
                     Err(_) => return Err(()),
                 };
-
-            },
+            }
             Err(_) => match fs::remove_file(&output_path) {
                 Ok(_) | Err(_) => return Err(()),
             },
@@ -189,14 +304,16 @@ fn detect_image_thermal_ocr(path: &str) -> Result<f32, OcrError> {
     } else if cfg!(target_os = "macos") {
         return Err(OcrError::NotSupportPlatform);
     }
-    if let Some(proj_dirs) = ProjectDirs::from("com", "com.miaostay.itmpc",  "PaddleOCR-json") {
+    if let Some(proj_dirs) = ProjectDirs::from("com", "com.miaostay.itmpc", "PaddleOCR-json") {
         let ppocr_dirs = proj_dirs.data_local_dir().join("PaddleOCR_json.exe");
-        println!("{}", ppocr_dirs.clone().into_os_string().into_string().unwrap());
+        println!(
+            "{}",
+            ppocr_dirs.clone().into_os_string().into_string().unwrap()
+        );
         // Lin: /home/alice/.config/barapp
         // Win: C:\Users\Alice\AppData\Roaming\Foo Corp\Bar App\config
         // Mac: /Users/Alice/Library/Application Support/com.Foo-Corp.Bar-App
-        let mut p = paddleocr::Ppocr::new(ppocr_dirs)
-        .unwrap();
+        let mut p = paddleocr::Ppocr::new(ppocr_dirs).unwrap();
         let ret = p.ocr(path).unwrap();
         match serde_json::from_str::<OcrResult>(&ret) {
             Ok(v) => {
@@ -209,17 +326,14 @@ fn detect_image_thermal_ocr(path: &str) -> Result<f32, OcrError> {
                         Err(_) => continue,
                     }
                 }
-            },
-            Err(_) => {
-                return Err(OcrError::NotFound)
             }
+            Err(_) => return Err(OcrError::NotFound),
         }
 
         return Err(OcrError::NotDetectd);
         // println!("{}", ret);
     }
     Err(OcrError::NotFound)
-
 }
 
 fn clip_picture(input_path: &PathBuf, output_path: &PathBuf) -> Result<(), ()> {
@@ -291,10 +405,6 @@ fn clip_picture(input_path: &PathBuf, output_path: &PathBuf) -> Result<(), ()> {
         },
         None => Err(()),
     }
-}
-
-fn download_ocr_lib() {
-    sevenz_rust::decompress_file("data/sample.7z", "data/sample").expect("complete");
 }
 
 #[cfg(test)]
