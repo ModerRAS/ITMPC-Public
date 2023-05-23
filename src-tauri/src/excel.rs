25f0f0d56c2937b7dac26bddd0dd65cf9111ec87
ModerRAS <adqewsx@gmail.com>
2023-05-23 09:11:19 +0800

    调整识别的行列，以对应表格的实际位置。
    (cherry picked from commit c3677defdbfc16ca2677bd6303577fced5a9b80b)

--- src-tauri/src/excel.rs
+++ src-tauri/src/excel.rs
@@ -46,9 +46,9 @@ fn write_excel_line(
     worksheet.write(row, 0, id).unwrap();
     worksheet.write(row, 1, interval_name).unwrap();
     worksheet.write(row, 2, device_name).unwrap();
-    worksheet.write(row, 3, voltage_level).unwrap();
-    worksheet.write(row, 8, measurement_image).unwrap();
-    worksheet.write(row, 13, thermal).unwrap();
+    worksheet.write(row, 4, voltage_level).unwrap();
+    worksheet.write(row, 10, measurement_image).unwrap();
+    worksheet.write(row, 15, thermal).unwrap();
 }
 
 #[tauri::command(rename_all = "snake_case")]
@@ -66,7 +66,7 @@ pub fn get_excel_lines(excel_path: &str) -> Result<Vec<ExcelData>, String> {
                             id: row[0].get_float().unwrap(),
                             interval_name: row[1].get_string().unwrap().to_string(),
                             device_name: row[2].get_string().unwrap().to_string(),
-                            voltage_level: row[3].get_string().unwrap().to_string(),
+                            voltage_level: row[4].get_string().unwrap().to_string(),
                             measurement_image: format!("{}.jpg", row[2].get_string().unwrap()),
                             thermal: -99999f32,
                         });
