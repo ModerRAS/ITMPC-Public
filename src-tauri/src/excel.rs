51b0c4b8ce1978824211cb7833efcd79d09b24ef
ModerRAS <adqewsx@gmail.com>
2023-05-25 15:34:12 +0800

    修复图片文件名中包含不支持的特殊字符的bug

--- src-tauri/src/excel.rs
+++ src-tauri/src/excel.rs
@@ -89,7 +89,7 @@ pub fn get_excel_lines(excel_path: &str) -> Result<Vec<ExcelData>, String> {
                             interval_name: row[1].get_string().unwrap_or_default().to_string(),
                             device_name: row[2].get_string().unwrap_or_default().to_string().replace("/", "").replace("\\", "").replace("?", "").replace("*", ""),
                             voltage_level: row[4].get_string().unwrap_or_default().to_string(),
-                            measurement_image: format!("{}.jpg", row[2].get_string().unwrap_or_default()),
+                            measurement_image: format!("{}.jpg", row[2].get_string().unwrap_or_default().to_string().replace("/", "").replace("\\", "").replace("?", "").replace("*", "")),
                             distance: row[14].get_float().unwrap_or(1.0f64),
                             thermal: row[15].get_float().unwrap_or(-99999f64),
                             normal_corresponding_point_temperature: row[16].get_float().unwrap_or(-99999f64),
