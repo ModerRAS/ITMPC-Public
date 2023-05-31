b19df3a6877dd2c876db7cf18792bb4449d16c73
ModerRAS <adqewsx@gmail.com>
2023-05-31 11:09:12 +0800

    代码结构优化

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,32 @@
-
 "use client";
 import React, { useEffect, useState } from "react";
-import Image from 'next/image'
-import { getVersion } from '@tauri-apps/api/app';
+import Image from "next/image";
+import { getVersion } from "@tauri-apps/api/app";
 
 import Link from "next/link";
 import ReturnToHome from "./util/home";
+import BaseContainer from "./util/container";
 
 export default function Home() {
   const [Version, setVersion] = useState([""]);
   useEffect(() => {
-    getVersion().then(v => setVersion(v))
+    getVersion().then((v) => setVersion(v));
   }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <BaseContainer>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Ver.{Version}
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/tauri_logo_light.svg"
-          alt="Next.js Logo"
-          width={70}
-          height={37}
-          priority
-        />
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/tauri_logo_light.svg"
+            alt="Next.js Logo"
+            width={70}
+            height={37}
+            priority
+          />
         </div>
       </div>
 
@@ -35,56 +35,6 @@ export default function Home() {
           红外测温实用工具
         </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-
-        <ReturnToHome></ReturnToHome>
-
-        <Link
-          href="/merge"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-        <h2 className={`mb-3 text-2xl font-semibold`}>
-            合并表格{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            将拆分的上传PMS的表格合并在一起
-          </p>
-        </Link>
-
-        <Link
-          href="/convert"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            修改文件名{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            常规上传PMS表格
-          </p>
-        </Link>
-
-        <Link
-          href="/convertTwo"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            修改文件名{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            二次专用表格转换
-          </p>
-        </Link>
-      </div>
-    </main>
-  )
+    </BaseContainer>
+  );
 }
